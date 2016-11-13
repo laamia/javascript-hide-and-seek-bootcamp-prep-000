8ba62f322156235b62baa5a978c7c3e77e95d431
Laamia Islam <laamia.islam@gmail.com>
2016-11-13 23:18:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function getFirstSelector(selector){
-  document.querySelector(selector)
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div div div div')
 }
